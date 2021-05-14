9f6099ee0b7f7be2ea03cfaffffb80c84514915f
Osman Cakmak <48910999+oxcakmak@users.noreply.github.com>
2021-05-15 02:30:49 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+* Special Thanks:
+* https://stackoverflow.com/questions/486896/adding-a-parameter-to-the-url-with-javascript
+* https://stackoverflow.com/questions/16941104/remove-a-parameter-to-the-url-with-javascript
+*/
+function insertParam(key, value) {
+    key = encodeURIComponent(key);
+    value = encodeURIComponent(value);
+    var kvp = document.location.search.substr(1).split("&");
+    let i=0;
+    for(; i<kvp.length; i++){
+        if (kvp[i].startsWith(key + "=")) {
+            let pair = kvp[i].split("=");
+            pair[1] = value;
+            kvp[i] = pair.join("=");
+            break;
+        }
+    }
+    if(i >= kvp.length){
+        kvp[kvp.length] = [key,value].join("=");
+    }
+    let params = kvp.join("&");
+    document.location.search = params;
+    url = document.location;
+}
+function removeParam(key, sourceURL) {
+    var rtn = sourceURL.split("?")[0],
+        param,
+        params_arr = [],
+        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+    if (queryString !== "") {
+        params_arr = queryString.split("&");
+        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+            param = params_arr[i].split("=")[0];
+            if (param === key) {
+                params_arr.splice(i, 1);
+            }
+        }
+        if (params_arr.length) rtn = rtn + "?" + params_arr.join("&");
+    }
+    location.href = rtn;    
+}
